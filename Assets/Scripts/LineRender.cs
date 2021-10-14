6aadcfe08aa0d65511c90c1c7c530d03901327f7
eishimoto <eishimoto@outlook.com>
2021-10-14 14:55:31 -0300

    line render

--- Assets/Scripts/LineRender.cs
+++ Assets/Scripts/LineRender.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class LineRender : MonoBehaviour
 {
     [SerializeField] private LineRenderer _renderer;
-    //[SerializeField] private EdgeCollider2D _collider;
+    [SerializeField] private EdgeCollider2D _collider;
 
     private readonly List<Vector2> _points = new List<Vector2>();
     void Start()
     {
-       // _collider.transform.position -= transform.position;
+        _collider.transform.position -= transform.position;
     }
 
 
     void Update()
     {
-        if (Input.GetMouseButtonUp(0)) Destroy(gameObject);
+       // if (Input.GetMouseButtonUp(0)) Destroy(gameObject);
     }
 
     public void SetPosition(Vector2 pos)
@@ -28,7 +28,7 @@ public class LineRender : MonoBehaviour
         _renderer.positionCount++;
         _renderer.SetPosition(_renderer.positionCount - 1, pos);
 
-       // _collider.points = _points.ToArray();
+        _collider.points = _points.ToArray();
     }
 
     private bool CanAppend(Vector2 pos)
