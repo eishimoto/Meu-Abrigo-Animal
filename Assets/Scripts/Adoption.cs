ee0dcbf5fc1acabd2d82c5a174e2b8381f0319c9
eishimoto <eishimoto@outlook.com>
2021-11-21 10:07:21 -0300

    save

--- Assets/Scripts/Adoption.cs
+++ Assets/Scripts/Adoption.cs
@@ -22,34 +22,33 @@ public class Adoption : MonoBehaviour
     {
         if (Stats.sick)
         {
-            //pets[0].SetActive(false);
             petsButtons[0].SetActive(false);
         }
         else if(!Stats.sick)
         {
             petsButtons[0].SetActive(true);
         }
+
         if (Stats2.sick)
         {
-           //pets[2].SetActive(false);
             petsButtons[2].SetActive(false);
         }
         else if (!Stats2.sick)
         {
             petsButtons[2].SetActive(true);
         }
+
         if (Stats3.sick)
         {
-            //pets[1].SetActive(false);
             petsButtons[1].SetActive(false);
         }
         else if (!Stats3.sick)
         {
             petsButtons[1].SetActive(true);
         }
+
         if (Stats4.sick)
         {
-            //pets[3].SetActive(false);
             petsButtons[3].SetActive(false);
         }
         else if (!Stats4.sick)
